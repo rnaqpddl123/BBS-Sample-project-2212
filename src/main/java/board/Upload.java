622ab6d970be493a3d8cb2e16f5a9cdeddada1e4
rnaqpddl123@gmail.com <rnaqpddl123@gmail.com>
2022-12-21 18:58:53 +0900

    fileupload

--- src/main/java/board/Upload.java
+++ src/main/java/board/Upload.java
@@ -1,4 +1,3 @@
-
 package board;
 
 import java.io.File;
@@ -14,17 +13,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+/**
+ * Servlet implementation class Upload
+ */
 @WebServlet("/board/uploads")
 @MultipartConfig(
 	    fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
 	    maxFileSize = 1024 * 1024 * 10,      // 10 MB
 	    maxRequestSize = 1024 * 1024 * 100   // 100 MB
-	)
+)
 public class Upload extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String tmpPath = "c:/Temp/upload";		// 여기서 설정하는것보다 config파일에서 설정하는게 더좋음
-        System.out.println("tmpPath: " + tmpPath);
+		String tmpPath = "c:/Temp/upload";
         File file = new File(tmpPath);
         if(!file.exists()) 
             file.mkdirs();
@@ -32,42 +33,38 @@ public class Upload extends HttpServlet {
         /* Receive file uploaded to the Servlet from the HTML5 form */
         request.setCharacterEncoding("utf-8");
         String param = request.getParameter("param");
-        System.out.println("param"+ param);
+        System.out.println("param: " + param);
         
         String fileName = null;
-        
-        //하나의 파일을 보낼때
-//        Part filePart = request.getPart("file");		// name으로 등록한것
+//        Part filePart = request.getPart("file");
 //        if (filePart == null) {
-//        	System.out.println("no files uploaded.");
+//        	System.out.println("No files uploaded.");
 //        } else {
 //        	fileName = filePart.getSubmittedFileName();
-//        	System.out.println("fileName" + fileName);
+//        	System.out.println("fileName: " + fileName);
 //        	
-//        	for (Part part : request.getParts()) {		// 파일 업로드
+//        	for (Part part : request.getParts()) {
 //                part.write(tmpPath + File.separator + fileName);
 //            }
 //        }
         	
-        // 여러개의 파일을 보낼때
-        Part filePart = null;
-		List<String> fileList = new ArrayList<>();
-		for (int i=1; i<=2; i++) {
-		    filePart = request.getPart("file" + i);		// name이 file1, file2
-		    if (filePart == null)
-		    	continue;
-		    fileName = filePart.getSubmittedFileName();
-		    System.out.println("file" + i + ": " + fileName);
-		    if (fileName == null || fileName.equals(""))
-		        continue;
-		    fileList.add(fileName);
-		    
-		    for (Part part : request.getParts()) {
-		    	System.out.println("여기가 이상" + fileName);
-		        part.write(tmpPath + File.separator + fileName);
-		    }
-		    response.getWriter().print("The file is uploaded sucessfully.");	
-		}
+        Part filePart = null;	
+        List<String> fileList = new ArrayList<>();
+        for (int i=1; i<=2; i++) {
+            filePart = request.getPart("file" + i);		// name이 file1, file2
+            if (filePart == null)
+            	continue;
+            fileName = filePart.getSubmittedFileName();
+            System.out.println("file" + i + ": " + fileName);
+            if (fileName == null || fileName.equals(""))
+                continue;
+            fileList.add(fileName);
+            
+            for (Part part : request.getParts()) {
+                part.write(tmpPath + File.separator + fileName);
+            }
+            response.getWriter().print("The file is uploaded sucessfully.");	
+        }
 	}
 
 }
--- src/main/java/misc/JSONSample.java
+++ src/main/java/misc/JSONSample.java
@@ -1,9 +1,38 @@
 package misc;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class JSONSample {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception {
+		// 내부 Object --> JSON String (stringfy)
+		JSONObject obj = new JSONObject();
+		JSONArray arr = new JSONArray();
+		
+		for (int i=1; i<=3; i++) {
+			JSONObject data = new JSONObject();
+			
+			data.put("이름", "인간_"+i);
+			data.put("나이", 10+i);
+			data.put("주소", "서울 중앙로" + i + "길");
+			arr.add(data);
+		}
+		obj.put("사람들", arr);
+		System.out.println(obj);
+	
+		// JSON String --> 내부 Object
+		JSONParser parser = new JSONParser();
+		JSONObject persons = (JSONObject) parser.parse(obj.toString());
+		JSONArray person_arr = (JSONArray) persons.get("사람들");		// ["사람들":[{},{},{}]}
+		JSONObject person = (JSONObject) person_arr.get(0);		// {"이름":"인간_1","주소":"서울 중앙로1길","나이":11}
+		System.out.println(person);
+		String name = (String) person.get("이름");
+		System.out.println(name);
+		person = (JSONObject) person_arr.get(1);
+		name = (String) person.get("이름");
+		System.out.println(name);
+		
 	}
-
 }
